7742e2cd4711ce9bf1d1029e1c1a0422529c24b4
Saqib Awan <saawan@trill.io>
2019-02-08 16:17:09 -0800

    initial set

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  public url : string;
+  options: InAppBrowserOptions = {
+    location: 'no',//Or 'no' 
+    hidden: 'no', //Or  'yes'
+    clearcache: 'yes',
+    clearsessioncache: 'yes',
+    zoom: 'no',//Android only ,shows browser zoom controls 
+    hardwareback: 'yes',
+    mediaPlaybackRequiresUserAction: 'no',
+    shouldPauseOnSuspend: 'no', //Android only 
+    closebuttoncaption: 'Close', //iOS only
+    disallowoverscroll: 'no', //iOS only 
+    toolbar: 'no', //iOS only 
+    enableViewportScale: 'no', //iOS only 
+    allowInlineMediaPlayback: 'no',//iOS only 
+    presentationstyle: 'pagesheet',//iOS only 
+    fullscreen: 'yes',//Windows only    
+  };
+  
+  constructor(private theInAppBrowser: InAppBrowser) { }
+
+  public openWithInAppBrowser(_url: string) {
+    if (_url != null) {
+      let target = "_blank";
+      this.theInAppBrowser.create(_url, target, this.options);
+    }
+  }
+}
